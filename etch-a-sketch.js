80390ac920053b4cc50078f580335e32c8fce879
Richard <53197263+ruizhelou@users.noreply.github.com>
2025-09-29 21:56:39 +1300

    Add button to enter custom grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,39 @@
-const container = document.querySelector(".container")
+function createGrid(gridSize) {
+    const container = document.querySelector(".container")
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
-    container.appendChild(row)
+    for(let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+        container.appendChild(row)
 
-    for(let j = 0; j < 16; j++) {
-        const col = document.createElement("div")
-        col.classList.add("col")
-        col.addEventListener("mouseover", event => col.style.backgroundColor = 'grey')
-        row.appendChild(col)
+        for(let j = 0; j < gridSize; j++) {
+            const col = document.createElement("div")
+            col.classList.add("col")
+            col.addEventListener("mouseover", event => col.style.backgroundColor = 'grey')
+            row.appendChild(col)
+        }
     }
 }
+
+function deleteGrid() {
+    const container = document.querySelector(".container")
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    // or container.innerHTML = ""
+}
+
+const changeGridButton = document.querySelector(".change-grid-button")
+changeGridButton.addEventListener("click", event => {
+    const gridSize = parseInt(prompt("New grid size (1-100):", "16"))
+    if(isNaN(gridSize)) {
+        alert("Grid size is not a number.")
+    } else if(gridSize < 1 || gridSize > 100) {
+        alert("Grid size is not a number must be between 1 and 100.")
+    } else {
+        deleteGrid()
+        createGrid(gridSize)
+    }
+})
+
+createGrid(16)
