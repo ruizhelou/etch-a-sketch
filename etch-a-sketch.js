459069524bfeec1af3e06967bf366926dc2c69e0
Richard <53197263+ruizhelou@users.noreply.github.com>
2025-09-29 21:07:37 +1300

    Add hover effect to cells

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,6 +8,7 @@ for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const col = document.createElement("div")
         col.classList.add("col")
+        col.addEventListener("mouseover", event => col.style.backgroundColor = 'grey')
         row.appendChild(col)
     }
 }
